5aed38c2437b9fa3d326aae3a9769ca5330b88bb
Dharun-069 <116252304+Dharun-069@users.noreply.github.com>
2023-03-09 18:14:18 +0530

    Add files via upload

---
+++ assets/JS/app.js
@@ -0,0 +1,27 @@
+
+/*get the number of characters*/
+// function charLength() {
+
+//     let charLength = document.getElementById('length').value;
+//     document.getElementById('result').innerHTML = charLength;
+//     return charLength;
+
+// }
+
+// document.getElementById('length').addEventListener('change', charLength)
+
+// let arr=[1,2,3]
+// let x;
+// for(let i=0 ; i<arr.length;i++){
+//    console.log(arr[i]);
+// }
+
+// let input =document.getElementById("length");
+
+let x=6;
+for(let i=0;i<x;i++){
+    console.log("A")
+}
+
+
+
